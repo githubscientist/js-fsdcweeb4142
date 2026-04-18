c00939ca465f4b94ca2cc207fe9ab624da18d456
githubscientist <sathish.datascientist@gmail.com>
2026-04-18 11:54:52 +0530

    multiple components --functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-// component -> a javascript function that returns a html block
+import Header from './components/Header';
+
 function App() {
     return (
         <>
-            <h1>Hello World!</h1>
+            <Header />
         </>
     )
 }
