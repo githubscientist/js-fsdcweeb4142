b4500421d6008dd6096aa4d11552bccfb8846cae
githubscientist <sathish.datascientist@gmail.com>
2026-04-25 12:14:20 +0530

    adding multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
-// state: a way to store and manage data in a component (component's memory)
-// state is used to store data that can change over time and affect the rendering of the component
-// in react, we can use useState hook to manage state in functional components
-// the useState is a function that returns an array
-// the array have a stateful value and a function to update it.
-
-// Note: We should not update or mutate the state directly.
-// Note: State change causes re-rendering.
 import { useState } from "react";
 
 const App = () => {
   const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
 
   const likeHandler = () => {
     setLikes(likes + 1);
   };
 
   const dislikeHandler = () => {
-    if (likes > 0) {
-      setLikes(likes - 1);
-    }
+    setDislikes(dislikes + 1);
   }
-
-  console.log(`likes:${likes}`);
 
   // when the like button is clicked, it will call likeHandler function
   return (
     <>
-      <h1>Likes: {likes}</h1>
       <button onClick={likeHandler}>
         <span className="material-symbols-outlined">
           thumb_up
         </span>
-      </button> &nbsp;
+        {likes}
+      </button>&nbsp;
       <button onClick={dislikeHandler}>
         <span className="material-symbols-outlined">
           thumb_down
         </span>
+        {dislikes}
       </button>
     </>
   )
