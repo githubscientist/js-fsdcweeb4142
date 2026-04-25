ecda350d80b239152468c56117625ee2427f3e08
githubscientist <sathish.datascientist@gmail.com>
2026-04-25 12:57:51 +0530

    complex state

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
 import { useState } from "react";
 
 const App = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
+  const [reactions, setReactions] = useState({
+    likes: 0,
+    dislikes: 0
+  });
 
   const likeHandler = () => {
-    setLikes(likes + 1);
+    setReactions({
+      ...reactions,
+      likes: reactions.likes + 1
+    });
   };
 
   const dislikeHandler = () => {
-    setDislikes(dislikes + 1);
+    setReactions({
+      ...reactions,
+      dislikes: reactions.dislikes + 1
+    })
   }
 
-  // when the like button is clicked, it will call likeHandler function
+  console.log(reactions);
+
   return (
     <>
       <button onClick={likeHandler}>
         <span className="material-symbols-outlined">
           thumb_up
         </span>
-        {likes}
+        {reactions.likes}
       </button>&nbsp;
       <button onClick={dislikeHandler}>
         <span className="material-symbols-outlined">
           thumb_down
         </span>
-        {dislikes}
+        {reactions.dislikes}
       </button>
     </>
   )
