4e1e21175c1443c84883b7a00e6865798fd2842b
githubscientist <sathish.datascientist@gmail.com>
2026-04-26 12:51:10 +0530

    state in class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { GrLike, GrDislike } from "react-icons/gr";
 
 export class App extends Component {
 
-    constructor(props) {
-        super(props);
+  state = {
+    likes: 0,
+    dislikes: 0
+  }
 
-        this.state = {
-            likes: 0
-        }
-    }
+  handleLike = () => {
+    this.setState({
+      ...this.state,
+      likes: this.state.likes + 1
+    })
+  }
 
-    handleLike = () => {
-        this.setState({
-            likes: this.state.likes + 1
-        })
-    }
+  handleDislike = () => {
+    this.setState({
+      ...this.state,
+      dislikes: this.state.dislikes + 1
+    })
+  }
 
-    render() {
-        return (
-            <>
-                <h1>Likes: {this.state.likes}</h1>
-                <button onClick={this.handleLike}>Like</button>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <button
+          onClick={this.handleLike}
+        >
+          <GrLike
+            style={{
+              color: "black",
+              fontSize: "30px",
+              padding: "5px"
+            }}
+          />
+          {this.state.likes}
+        </button>
+        &nbsp;
+        <button
+          onClick={this.handleDislike}
+        >
+          <GrDislike
+            style={{
+              color: "black",
+              fontSize: "30px",
+              padding: "5px"
+            }}
+          />
+          {this.state.dislikes}
+        </button>
+      </>
+    )
+  }
 }
 
 export default App;
