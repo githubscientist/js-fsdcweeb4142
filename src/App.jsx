3656ae5ccef1457ce3ce386e1d15aba79b1b62f8
githubscientist <sathish.datascientist@gmail.com>
2026-04-26 12:05:29 +0530

    css in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // controlled input: an input element whose value is controlled by React state.
 
 import { useState } from "react";
+import './App.css';
 
 const App = () => {
   const [email, setEmail] = useState("");
@@ -18,18 +19,34 @@ const App = () => {
     event.target.email.focus();
   }
 
+  // const formStyles = {
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   gap: 10,
+  //   border: "1px solid lightgray",
+  //   padding: "32px"
+  // };
+
+  // inline styling
   return (
     <>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
+      <form
+        onSubmit={handleLogin}
+        className="formLogin"
+      >
+        <h1>Login</h1>
         <div>
           <input
             name="email"
             type="email"
-            placeholder="email..."
+            placeholder="Type your email..."
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             required
+            style={{
+              height: "30px",
+              paddingLeft: "16px"
+            }}
           />
         </div>
 
@@ -37,14 +54,21 @@ const App = () => {
           <input
             name="password"
             type="password"
-            placeholder="password..."
+            placeholder="Type your password..."
             value={password}
             onChange={e => setPassword(e.target.value)}
             required
+            style={{
+              height: "30px",
+              paddingLeft: "16px"
+            }}
           />
         </div>
 
-        <button type="submit">
+        <button
+          type="submit"
+          className="btnLogin"
+        >
           Login
         </button>
       </form>
