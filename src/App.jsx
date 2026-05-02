c1ffbdc03f91a290d5c02955393b1d84498e52d3
githubscientist <sathish.datascientist@gmail.com>
2026-05-02 11:50:37 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import './App.css';
+
 const App = () => {
 
   const recipes = [
-    'Biryani',
-    'Pulao',
-    'Fried Rice',
-    'Noodles',
-    'Pasta',
-    'Pizza',
-    'Burger',
-    'Sandwich',
-    'Salad',
-    'Soup'
+    {
+      name: 'Pasta',
+      ingredients: ['Pasta', 'Tomato Sauce', 'Cheese'],
+      author: 'John Doe',
+      cookingTime: '30 minutes',
+      difficulty: 'Easy',
+      isFavourite: true
+    },
+    {
+      name: 'Chicken Curry',
+      ingredients: ['Chicken', 'Curry Powder', 'Coconut Milk'],
+      author: 'Jane Smith',
+      cookingTime: '45 minutes',
+      difficulty: 'Medium',
+      isFavourite: false
+    },
+    {
+      name: 'Chocolate Cake',
+      ingredients: ['Flour', 'Sugar', 'Cocoa Powder', 'Eggs'],
+      author: 'Emily Johnson',
+      cookingTime: '1 hour',
+      difficulty: 'Hard',
+      isFavourite: false
+    }
   ];
 
   return (
@@ -20,11 +36,40 @@ const App = () => {
         {
           recipes
             .map((recipe, index) => {
-              return <li key={index}>{recipe}</li>
+              return <li key={index}>
+                <div className="recipe-card">
+                  <header>
+                    <h2>{recipe.name}</h2>
+                    <span className="material-symbols-outlined"
+                      style={{
+                        "fontVariationSettings":
+                          `'FILL' ${recipe.isFavourite ? 1 : 0},
+                        'wght' 400,
+                        'GRAD' 0,
+                        'opsz' 24`,
+                        fontSize: '32px',
+                      }}>
+                      favorite
+                    </span>
+                  </header>
+                  <hr></hr>
+                  <p><strong>Author:</strong> {recipe.author}</p>
+                  <p><strong>Cooking Time:</strong> {recipe.cookingTime}</p>
+                  <p><strong>Difficulty:</strong> {recipe.difficulty}</p>
+                  <p><strong>Ingredients:</strong></p>
+                  <ul>
+                    {
+                      recipe.ingredients.map((ingredient, idx) => {
+                        return <li key={idx}>{ingredient}</li>
+                      })
+                    }
+                  </ul>
+                </div>
+              </li>
             }
             )
         }
-      </ul>
+      </ul >
     </>
   )
 }
