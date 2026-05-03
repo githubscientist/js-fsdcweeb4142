1404e648aaac93c4fbd607487df3664a26edefa8
githubscientist <sathish.datascientist@gmail.com>
2026-05-03 13:02:09 +0530

    react router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Todo from "./components/Todo";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Todos from "./components/Todos";
+
+// create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Todos />
+  }
+]);
 
 const App = () => {
-
-  const [todos, setTodos] = useState([]);
-
-  // 3. with dependencies list
-  useEffect(() => {
-    // runs when the component is rendered for the first time alone.
-    axios.get(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
-      .then(response => setTodos(response.data));
-  }, []);
-
-  return (
-    <>
-      <h1>Todos</h1>
-      <ul>
-        {
-          todos
-            .map(todo => {
-              return <Todo
-                key={todo.id}
-                todo={todo}
-              />
-            })
-        }
-      </ul>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Todo from "./Todo";
+
+const Todos = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    // 3. with dependencies list
+    useEffect(() => {
+        // runs when the component is rendered for the first time alone.
+        axios.get(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
+            .then(response => setTodos(response.data));
+    }, []);
+
+    return (
+        <>
+            <h1>Todos</h1>
+            <ul>
+                {
+                    todos
+                        .map(todo => {
+                            return <Todo
+                                key={todo.id}
+                                todo={todo}
+                            />
+                        })
+                }
+            </ul>
+        </>
+    )
+}
+
+export default Todos;
