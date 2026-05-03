07c9d3ee6415945b2eef1cd70490ac102f0fbf9e
githubscientist <sathish.datascientist@gmail.com>
2026-05-03 10:32:55 +0530

    useEffect --with dependencies list

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { useEffect, useState } from "react";
 const App = () => {
 
   const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
 
-  // 2. with empty dependencies list
+  // 3. with dependencies list
   useEffect(() => {
     // runs when the component is rendered for the first time alone.
     fetch(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
       .then(response => response.json())
       .then(todos => console.log(todos));
-  }, []);
+  }, [count1, count]);
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Fetch Todos</button>
+      <button onClick={() => setCount(count + 1)}>Fetch Todos</button> &nbsp;
+      <button onClick={() => setCount1(count1 + 1)}>Change State</button>
     </>
   )
 }
