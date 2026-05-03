a72635247a0e216e5663da4b6118b67e7ff2751b
githubscientist <sathish.datascientist@gmail.com>
2026-05-03 11:30:27 +0530

    useReducer with Like Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LIKE':
+      return state + 1;
+    case 'DISLIKE':
+      return state - 1;
+    case 'GOLDEN_LIKE':
+      return state + action.payload;
+    default:
+      return state;
+  }
+}
 
 const App = () => {
-  const [likes, setLikes] = useState(0);
+
+  const [likes, setLikes] = useReducer(reducer, 0);
 
   return (
     <>
       <h1>Likes: {likes}</h1>
-      <button onClick={() => setLikes(likes + 1)}>Like</button>
+      <button onClick={() => setLikes({ type: 'LIKE' })}>Like</button> &nbsp;
+      <button onClick={() => setLikes({ type: 'DISLIKE' })}>Dislike</button> &nbsp;
+      <button onClick={() => setLikes({ type: 'GOLDEN_LIKE', payload: 100 })}>Golden Like</button>
     </>
   )
 }
