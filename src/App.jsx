c7159d456aa04c5b669feb39744a050b5edceec8
githubscientist <sathish.datascientist@gmail.com>
2026-05-03 12:05:01 +0530

    normal variables --doesnt persist across re-renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import { useReducer, useState } from "react";
+// import { useState } from "react";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'LIKE':
-      return state + 1;
-    case 'DISLIKE':
-      return state - 1;
-    case 'GOLDEN_LIKE':
-      return state + action.payload;
-    default:
-      return state;
-  }
-}
+// const App = () => {
+
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <h1>Count: {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Inc</button>
+//     </>
+//   )
+// }
+
+// export default App;
+
+import { useState } from "react";
 
 const App = () => {
 
-  const [likes, setLikes] = useReducer(reducer, 0);
+  let count = 0;
+  let [state, setState] = useState(true);
+
+  const handleCount = () => {
+    count = count + 1;
+    console.log(count);
+  }
+
+  console.log(`current state: ${state}`);
 
   return (
     <>
-      <h1>Likes: {likes}</h1>
-      <button onClick={() => setLikes({ type: 'LIKE' })}>Like</button> &nbsp;
-      <button onClick={() => setLikes({ type: 'DISLIKE' })}>Dislike</button> &nbsp;
-      <button onClick={() => setLikes({ type: 'GOLDEN_LIKE', payload: 100 })}>Golden Like</button>
+      <h1>Count: {count}</h1>
+      <button onClick={handleCount}>Inc</button> &nbsp;
+      <button onClick={() => setState(!state)}>Change State</button>
     </>
   )
 }
