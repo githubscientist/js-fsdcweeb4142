9fc6c368beec62a004872b546053e2db30fc949a
githubscientist <sathish.datascientist@gmail.com>
2026-05-03 10:43:14 +0530

    axios + list re-rendering

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# useEffect Hook
+## useEffect Hook
 
 - `useEffect` is a hook in React that allows you to perform side effects in function components. It serves the same purpose as lifecycle methods like `componentDidMount`, `componentDidUpdate`, and `componentWillUnmount` in class components.
 - side effects can include things like fetching data from an API, subscribing to events, or manually manipulating the DOM.
 
-## The ways to use `useEffect`:
+### The ways to use `useEffect`:
 
 1. **Without dependencies**: The effect will run after every render.
 2. **With an empty dependency array**: The effect will run only once, after the initial render.
 3. **With specific dependencies**: The effect will run only when the specified dependencies change.
 
+## Axios
+
+- `Axios` is a popular JavaScript library used to make HTTP requests from the browser or Node.js. 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
+  const [todos, setTodos] = useState([]);
 
   // 3. with dependencies list
   useEffect(() => {
     // runs when the component is rendered for the first time alone.
-    fetch(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
-      .then(response => response.json())
-      .then(todos => console.log(todos));
-  }, [count1, count]);
+    axios.get(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
+      .then(response => setTodos(response.data));
+  }, []);
+
+  console.log(todos);
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Fetch Todos</button> &nbsp;
-      <button onClick={() => setCount1(count1 + 1)}>Change State</button>
+      <h1>Todos</h1>
+      <ul>
+        {
+          todos
+            .map(todo => (
+              <li key={todo.id}>{todo.title}</li>
+            ))
+        }
+      </ul>
     </>
   )
 }
