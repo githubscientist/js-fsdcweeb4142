053f4b11847bc11fccdc66bf9084dd51372fcafa
githubscientist <sathish.datascientist@gmail.com>
2026-05-17 11:58:44 +0530

    basic cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Todo from "./components/Todo";
-
 const App = () => {
-
-  const [todos, setTodos] = useState([]);
-
-  // 3. with dependencies list
-  useEffect(() => {
-    // runs when the component is rendered for the first time alone.
-    axios.get(`https://69f6d332a72f01a951b98c80.mockapi.io/todos`)
-      .then(response => setTodos(response.data));
-  }, []);
-
   return (
-    <>
-      <h1>Todos</h1>
-      <ul>
-        {
-          todos
-            .map(todo => {
-              return <Todo
-                key={todo.id}
-                todo={todo}
-              />
-            })
-        }
-      </ul>
-    </>
+    <div>App</div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-// remove the comment line below to enable tailwind css
-// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <div>
