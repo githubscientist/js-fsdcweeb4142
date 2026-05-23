53ef992202d43b722804f5b6faf50b9aee94f3a2
githubscientist <sathish.datascientist@gmail.com>
2026-05-23 11:40:27 +0530

    drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,61 @@
-import { createContext } from "react";
-import Child1 from "./components/Child1";
-
-// 1. create a context
-export const MessageContext = createContext();
+import { DragDropProvider } from '@dnd-kit/react';
+import { useState } from 'react';
+import SortableItem from './SortableItem';
+import { isSortable } from '@dnd-kit/react/sortable';
 
 const App = () => {
-  // parent component which has the data
-  const message = "Hello from App component";
 
-  // 2. Provide the context value to the child components
+  const [items, setItems] = useState([
+    { id: 1, content: 'Apple' },
+    { id: 2, content: 'Banana' },
+    { id: 3, content: 'Cherry' },
+    { id: 4, content: 'Orange' },
+    { id: 5, content: 'Grapes' },
+  ]);
+  
   return (
-    <MessageContext.Provider value={message}>
-      <div>
-        <Child1 />
-      </div>
-    </MessageContext.Provider>
+    <div>
+      <h1>Fruits</h1>
+
+      <DragDropProvider
+        onDragEnd={
+          (event) => {
+            if (event.canceled) return;
+
+            const { source } = event.operation;
+
+            if (isSortable(source)) {
+              const { initialIndex, index } = source;
+
+              if (initialIndex !== index) {
+                setItems((items) => {
+                  const newItems = [...items];
+
+                  const [removed] = newItems.splice(initialIndex, 1);
+                  newItems.splice(index, 0, removed);
+
+                  return newItems;
+                })
+              }
+            }
+          }
+        }
+      >
+        <ul>
+          {
+            items.map((item, index) => (
+              <SortableItem
+                key={item.id}
+                id={item.id}
+                index={ index }
+              >
+                {item.content}
+              </SortableItem>
+            ))
+          }
+        </ul>
+      </DragDropProvider>
+    </div>
   )
 }
 
