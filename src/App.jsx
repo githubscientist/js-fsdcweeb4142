1dc2972b25fa9d69dbf743466f7ac4b802ea7fb9
githubscientist <sathish.datascientist@gmail.com>
2026-05-24 10:37:57 +0530

    state management: useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend
-} from "chart.js";
-
-ChartJS.register(
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend
-);
+import { useState } from "react";
 
 const App = () => {
-  const data = {
-    labels: ["HTML", "CSS", "JavaScript", "React"],
-    datasets: [
-      {
-        label: "Marks",
-        data: [85, 90, 80, 95],
-        backgroundColor: ["red", "blue", "yellow", "green"],
-      }
-    ]
-  }
-
-  const options = {
-    responsive: true,
-  }
-
-
+  const [likes, setLikes] = useState(0);
+  
   return (
     <div>
-      <h2>Student Skill Marks</h2>
-      <Bar
-        data={data}
-        options={options}
-      />
+      <h1>Likes: {likes}</h1>
+      <button onClick={() => setLikes(likes + 1)}>Like</button>
     </div>
   )
 }
