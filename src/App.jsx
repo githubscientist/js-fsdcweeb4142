8a3b03815704bc04c5f33b87a906db70b375fe6b
githubscientist <sathish.datascientist@gmail.com>
2026-05-31 11:12:44 +0530

    cleanup --done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import { useReducer, useState } from "react";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'LIKE':
-      return state + 1;
-    case 'DISLIKE':
-      return state - 1;
-    case 'GOLDEN_LIKE':
-      return state + action.payload;
-    default:
-      return state;
-  }
-}
-
 const App = () => {
-
-  const [likes, setLikes] = useReducer(reducer, 0);
-
   return (
-    <>
-      <h1>Likes: {likes}</h1>
-      <button onClick={() => setLikes({ type: 'LIKE' })}>Like</button> &nbsp;
-      <button onClick={() => setLikes({ type: 'DISLIKE' })}>Dislike</button> &nbsp;
-      <button onClick={() => setLikes({ type: 'GOLDEN_LIKE', payload: 100 })}>Golden Like</button>
-    </>
+    <div>
+      <h1>Hello World!</h1>
+    </div>
   )
 }
 
